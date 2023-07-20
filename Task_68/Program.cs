5c51b2b287710c44f938ae2cbe8fdc56ca3da6c4
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-07-20 19:33:36 +0300

    Task_68

---
+++ Task_68/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+*/
+
+Console.Clear();
+int numberFromUser1 = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+int numberFromUser2 = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int number) && number >= 0)
+            return number;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int Accerman(int a, int b)
+{
+    if(a == 0)
+    {
+        return b + 1;
+    }
+    if(a > 0 && b == 0) 
+    {
+        return Accerman(a - 1, 1);
+    }
+    if(a > 0 && b > 0) 
+    {
+        return Accerman(a - 1, Accerman(a, b - 1));
+    }
+    return 0;
+}
+
+Console.WriteLine(Accerman(numberFromUser1, numberFromUser2));
