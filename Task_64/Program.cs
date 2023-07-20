c9ad0309256d76417393999fd5ec422fdfe081ed
Mahmiev Aslbek <mahmiyev.aslbek@gmail.com>
2023-07-20 19:29:45 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+*/
+
+Console.Clear();
+int numberFromUser = GetNumberFromUser("Введите число: ", "Ошибка ввода");
+
+void PoserRec(int a)
+{
+    if (a > 0)
+    {
+        Console.Write($"{a}, ");
+        PoserRec(a - 1);
+    }
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int number))
+            return number;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+PoserRec(numberFromUser);
